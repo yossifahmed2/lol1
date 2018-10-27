3fc2c0ed7d9882d3f01228d31a3b0d03a1797d20
yossifahmed2 <44387378+yossifahmed2@users.noreply.github.com>
2018-10-27 20:04:18 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log("Welcome Again By : Kahrbaa <3");
+    client.user.setStatus('idle');
+});
+
+
+  client.login('NTAxOTI3MzAxNzgxNTIwMzg1.DqhFeg.7ElwFMG4M2-o1blzveWdaQ0ZcUQ');
